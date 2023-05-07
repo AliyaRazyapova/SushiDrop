1e217df52951d4a2e9be4129ba3877e7b6445c64
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-08 01:06:45 +0300

    [edit] urls

--- sushidrop/urls.py
+++ sushidrop/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from core.views import LoginView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/core/', include('core.urls'))
+    path('api/core/', include('core.urls')),
 ]
