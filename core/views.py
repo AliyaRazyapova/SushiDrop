4072be1208692e5ec93456704a79eb2bb85923b3
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-07 13:46:16 +0300

    [fix] views

--- core/views.py
+++ core/views.py
@@ -1,16 +1,14 @@
-from rest_framework import generics, status
+from django.contrib.auth.hashers import make_password
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import UserSerializer
 
 
-class RegisterAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(password=make_password(serializer.validated_data['password']))
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
