f29bfe6bc603cb9370a1a4b57d2fe6033f7fab69
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-08 22:38:24 +0300

    [add] OAuth LoginForm

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -13,6 +13,9 @@
       <button type="submit">Войти</button>
     </form>
     <p v-if="message">{{ message }}</p>
+    <div>
+    <button @click="authorize">Авторизация через VK</button>
+  </div>
   </div>
 </template>
 <script>
@@ -37,11 +40,28 @@ export default {
         if (response.status === 200) {
           this.message = 'Вы успешно авторизовались!'
           localStorage.setItem('token', response.data.token)
-          this.$router.push('/home')
+          this.$router.push('/')
         }
       } catch (error) {
         this.message = 'Ошибка авторизации: ' + error.response.data.message
       }
+    },
+    authorize() {
+      const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51639163&redirect_uri=http://localhost:8000/auth/vk/callback&response_type=code&scope=email`;
+
+      window.location.href = vkAuthUrl;
+
+      window.addEventListener('message', async (event) => {
+        if (event.origin !== 'https://oauth.vk.com') {
+          return;
+        }
+
+        const code = event.data.code;
+        const tokenResponse = await axios.post(`https://oauth.vk.com/access_token?client_id=51639163&client_secret=N2eKlfa7qpe7NC20car5&redirect_uri=http://localhost:8000/auth/vk/callback&code=${code}`);
+        const userResponse = await axios.get(`https://api.vk.com/method/users.get?fields=email&access_token=${tokenResponse.data.access_token}&v=5.130&lang=en`);
+
+        console.log(userResponse.data.response[0]);
+      });
     }
   }
 }
