04480515b1efb029202d39e8da0afba6b91c93ac
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-09 13:38:47 +0300

    [add] headerbuttons in LoginView

---
+++ frontend/src/components/elements/HeaDer.vue
@@ -0,0 +1,18 @@
+<template>
+  <lo-go />
+  <header-buttons />
+</template>
+
+<script>
+import HeaderButtons from "@/components/elements/HeaderButtons";
+import LoGo from "@/components/elements/LoGo";
+
+export default {
+name: "HeaDer",
+  components: {LoGo, HeaderButtons}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/elements/HeaderButtons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button v-if="isLoggedIn" @click="goToProfile">Профиль</button>
+    <button v-else @click="goToLogin">Войти</button>
+    <button @click="goToCart">Корзина</button>
+    <button v-if="isLoggedIn" @click="logout">Выйти</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderButtons',
+  computed: {
+    isLoggedIn() {
+      return localStorage.getItem('token') !== null;
+    }
+  },
+  methods: {
+    goToProfile() {
+      this.$router.push('/profile');
+    },
+    goToCart() {
+      this.$router.push('/cart');
+    },
+    goToLogin() {
+      this.$router.push('/login');
+    },
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push('/');
+      location.reload();
+    }
+  }
+}
+</script>
