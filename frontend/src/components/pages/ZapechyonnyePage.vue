3ea863e0a7d49c38c85be93e6cb184bcf7226815
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-13 15:01:57 +0300

    [add] ZapechyonnyePage

---
+++ frontend/src/components/pages/ZapechyonnyePage.vue
@@ -0,0 +1,39 @@
+<template>
+  <hea-der></hea-der>
+  <category-list></category-list>
+  <div class="main">
+    <div class="title">
+      Запечённые
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+
+export default {
+  name: "ZapechyonnyePage",
+  components: {CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+  }
+
+  .title {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 36px;
+    color: #212121;
+
+    margin: auto;
+  }
+</style>
