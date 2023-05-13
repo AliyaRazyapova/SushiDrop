16b9af715ece24786419cf285d9a46f3d1a77d46
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-13 13:46:19 +0300

    [add] MainPage

---
+++ frontend/src/components/pages/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <hea-der></hea-der>
+  <category-list></category-list>
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+
+export default {
+  name: "MainPage",
+  components: {CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+
+</style>
