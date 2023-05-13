6f860dcff131e5b776d9c2a2556ef0a5f457e9d4
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-13 14:16:34 +0300

    [add] title in MainPage

--- frontend/src/components/pages/MainPage.vue
+++ frontend/src/components/pages/MainPage.vue
@@ -1,6 +1,11 @@
 <template>
   <hea-der></hea-der>
   <category-list></category-list>
+  <div class="main">
+    <div class="title">
+      SushiDrop
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,5 +19,21 @@ export default {
 </script>
 
 <style scoped>
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+  }
 
+  .title {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 36px;
+    color: #212121;
+
+    margin: auto;
+  }
 </style>
