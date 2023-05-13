f1442da841dbd9a37048e16fae3802875ca48598
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-13 14:20:38 +0300

    [fix] router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import RegistrationForm from "@/components/forms/RegistrationForm";
 import LoginForm from "@/components/forms/LoginForm";
 import UserProfile from "@/components/forms/UserProfile";
 import MainPage from "@/components/pages/MainPage";
+import PremiumPage from "@/components/pages/PremiumPage";
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: '/profile',
     name: 'profile',
     component: UserProfile
+  },
+  {
+    path: '/premium',
+    name: 'premium',
+    component: PremiumPage
   }
 ]
 
