3a4b997772dfd70a0a03189393b9ea8290e440e1
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-14 16:59:55 +0300

    [fix] style in Product

--- frontend/src/components/elements/ProDuct.vue
+++ frontend/src/components/elements/ProDuct.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="product">
-    <h2>{{ product.name }}</h2>
     <img :src="product.image" alt="product image">
-    <p>{{ product.description }}</p>
-    <p>{{product.gramms}}</p>
-    <p>{{ product.price }}</p>
+    <div class="name">{{ product.name }}</div>
+    <div class="gramms_price">
+      <div class="gramms">{{product.gramms}} гр.</div>
+      <div class="price_1">
+        <div class="price">{{ product.price }} ₽</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,3 +41,68 @@ export default {
   }
 }
 </script>
+
+<style>
+  .product {
+    width: 250px;
+    height: 270px;
+  }
+
+  img {
+    width: 250px;
+    height: 184px;
+  }
+
+  .name {
+    font-family: 'PT Sans', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 21px;
+
+    color: #000000;
+
+    margin-left: 15px;
+  }
+
+  .gramms_price {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .gramms {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 11.3208px;
+    line-height: 17px;
+
+    color: #7C909C;
+
+    margin: 16px 0 0 15px;
+  }
+
+  .price_1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: right;
+
+    margin: 15px 15px 0 auto;
+
+    width: 93.93px;
+    height: 27.27px;
+
+    border: 1.5px solid #F52341;
+    border-radius: 3.78742px;
+  }
+
+  .price {
+    font-family: 'Poppins', sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 13.6347px;
+    line-height: 20px;
+    color: #F52341;
+  }
+</style>
