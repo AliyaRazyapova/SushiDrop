4a552da374268bc93f8b4d18ed57de5640ff0969
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-14 15:33:28 +0300

    [add] gramms

--- products/models.py
+++ products/models.py
@@ -9,5 +9,6 @@ class Product(models.Model):
     category = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    gramms = models.IntegerField()
     price = models.FloatField()
     image = models.ImageField(upload_to='img/')
