41ee2712aebc1822ea08e6f9d900e0a6a20cd2a9
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-14 14:29:46 +0300

    [fix] urls in products

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/create/', views.create_product, name='create_product'),
-    path('categories/', views.list_categories, name='list_categories')
+    path('categories/', views.list_categories, name='list_categories'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
