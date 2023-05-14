9d46aec9c29743bfb7c7ffd1b0ba40e7d87aa316
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-14 14:27:42 +0300

    [add] ProductList

---
+++ frontend/src/components/elements/ProductList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="product-list">
+    <h1>Products</h1>
+    <div v-for="product in products" :key="product.id">
+      <pro-duct :product-id="product.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ProDuct from "@/components/elements/ProDuct";
+
+export default {
+  name: 'ProductList',
+  components: {
+    ProDuct
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.getProducts()
+  },
+  methods: {
+    async getProducts() {
+      try {
+        const response = await fetch('http://localhost:8000/api/products/')
+        const data = await response.json()
+        this.products = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
