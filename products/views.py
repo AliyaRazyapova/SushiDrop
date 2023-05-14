5f7cc4a6aebd028b625eea396d44b97a9c6b0e4a
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-14 15:06:36 +0300

    [fix] views in products

--- products/views.py
+++ products/views.py
@@ -26,3 +26,26 @@ def list_categories(request):
     categories = CategoryProduct.objects.all()
     data = [{'id': category.id, 'name': category.name} for category in categories]
     return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+def product_list(request):
+    products = Product.objects.all()
+    data = [{'id': p.id, 'name': p.name, 'description': p.description, 'price': p.price, 'image': p.image.url} for p in products]
+    return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+        data = {
+            'id': product.id,
+            'name': product.name,
+            'description': product.description,
+            'price': product.price,
+            'image': product.image.url,
+        }
+        return JsonResponse(data)
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product not found'}, status=404)
