7982ca7823c5c1ec88f0758442477f52519bf5ef
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 20:01:32 +0300

    [fix] NaboryPage

--- frontend/src/components/pages/NaboryPage.vue
+++ frontend/src/components/pages/NaboryPage.vue
@@ -1,36 +1,28 @@
 <template>
-  <div class="product-list">
-    <div v-for="product in products" :key="product.id">
-      <pro-duct :product-id="product.id" />
+  <hea-der />
+  <category-list />
+  <div class="main">
+    <div class="title">
+      Наборы
     </div>
   </div>
+  <div class="product-list">
+    <product-list-page URL="http://localhost:8000/api/products/nabory/" />
+  </div>
 </template>
+
 <script>
-import ProDuct from "@/components/elements/ProDuct";
+import "@/assets/pages.css"
+import ProductListPage from "@/components/elements/ProductList";
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
 
 export default {
   name: 'NaboryPage',
   components: {
-    ProDuct
-  },
-  data() {
-    return {
-      products: []
-    }
-  },
-  mounted() {
-    this.getProducts()
-  },
-  methods: {
-    async getProducts() {
-      try {
-        const response = await fetch('http://localhost:8000/api/nabory/')
-        const data = await response.json()
-        this.products = data
-      } catch (error) {
-        console.error(error)
-      }
-    }
+    CategoryList,
+    HeaDer,
+    ProductListPage
   }
 }
 </script>
