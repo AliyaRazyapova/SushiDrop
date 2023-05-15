8d8ca97655c1d5de6fed4303578b4fa55ada6564
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 15:12:11 +0300

    [fix] urls in products

--- products/urls.py
+++ products/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +9,4 @@ urlpatterns = [
     path('categories/', views.list_categories, name='list_categories'),
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
