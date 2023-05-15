adad9a00a1900699ffedb247cc3c5d345cfb1bd0
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 15:15:33 +0300

    [add] NaboryPage

--- frontend/src/components/pages/NaboryPage.vue
+++ frontend/src/components/pages/NaboryPage.vue
@@ -1,39 +1,36 @@
 <template>
-  <hea-der></hea-der>
-  <category-list></category-list>
-  <div class="main">
-    <div class="title">
-      Наборы
+  <div class="product-list">
+    <div v-for="product in products" :key="product.id">
+      <pro-duct :product-id="product.id" />
     </div>
   </div>
 </template>
-
 <script>
-import HeaDer from "@/components/elements/HeaDer";
-import CategoryList from "@/components/elements/CategoryList";
+import ProDuct from "@/components/elements/ProDuct";
 
 export default {
-  name: "NaboryPage",
-  components: {CategoryList, HeaDer}
+  name: 'NaboryPage',
+  components: {
+    ProDuct
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.getProducts()
+  },
+  methods: {
+    async getProducts() {
+      try {
+        const response = await fetch('http://localhost:8000/api/nabory/')
+        const data = await response.json()
+        this.products = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
 }
 </script>
-
-<style scoped>
-  .main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 80px;
-  }
-
-  .title {
-    font-family: 'Poppins', sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 36px;
-    color: #212121;
-
-    margin: auto;
-  }
-</style>
