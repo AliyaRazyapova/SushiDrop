46712e7e2c643d7f35259b31cfaaba785fb0447e
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 15:12:24 +0300

    [add] urls in products

--- products/urls.py
+++ products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('categories/', views.list_categories, name='list_categories'),
     path('products/', views.product_list, name='product_list'),
     path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('nabory/', views.nabory_list, name='nabory_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
