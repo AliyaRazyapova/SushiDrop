f7c2278b899e1c6df7fc4f4b2953852e9417c5a7
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 12:51:26 +0300

    [fix] models in products

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CategoryProduct(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
 
 class Product(models.Model):
