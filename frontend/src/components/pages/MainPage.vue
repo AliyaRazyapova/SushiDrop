2fdadf329e7f6ea31c2a71124ac98d7615089bb6
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-15 20:00:11 +0300

    [fix] MainPage

--- frontend/src/components/pages/MainPage.vue
+++ frontend/src/components/pages/MainPage.vue
@@ -1,41 +1,28 @@
 <template>
-  <hea-der></hea-der>
-  <category-list></category-list>
+  <hea-der />
+  <category-list />
   <div class="main">
     <div class="title">
       SushiDrop
     </div>
   </div>
-  <product-list />
+  <div class="product-list">
+    <product-list URL="http://localhost:8000/api/products/" />
+  </div>
 </template>
 
 <script>
+import '@/assets/pages.css';
 import HeaDer from "@/components/elements/HeaDer";
 import CategoryList from "@/components/elements/CategoryList";
 import ProductList from "@/components/elements/ProductList";
 
 export default {
   name: "MainPage",
-  components: {ProductList, CategoryList, HeaDer}
+  components: {
+    ProductList,
+    CategoryList,
+    HeaDer
+  }
 }
 </script>
-
-<style scoped>
-  .main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 80px;
-  }
-
-  .title {
-    font-family: 'Poppins', sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 36px;
-    color: #212121;
-
-    margin: auto;
-  }
-</style>
