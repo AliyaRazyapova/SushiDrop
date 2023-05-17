298e783344d82f8bcbe3d4ac75bc4b013314168a
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-17 22:00:38 +0300

    [add] ProductPage

---
+++ frontend/src/components/pages/ProductPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <hea-der />
+  <category-list />
+  <product-profile />
+</template>
+
+<script>
+import ProductProfile from "@/components/elements/ProductProfile";
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+
+export default {
+  name: "ProductPage",
+  components: {
+    CategoryList,
+    HeaDer,
+    ProductProfile
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
