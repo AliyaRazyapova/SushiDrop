d2f6c7813b412369a00b21cc39a6b9ad926911c7
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-18 16:46:14 +0300

    [add] get_cart_items in views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Cart
+from .serializers import CartSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_cart_items(request):
+    user = request.user
+    cart_items = Cart.objects.filter(user=user)
+    serializer = CartSerializer(cart_items, many=True)
+    return Response(serializer.data)
