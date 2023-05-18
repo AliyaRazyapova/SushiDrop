64c11ee46df60881307b543c01ea4b47f4279b9c
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-18 17:45:51 +0300

    [add] remove_from_cart in urls

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from cart import views
 
 urlpatterns = [
     path('get/', views.get_cart_items, name='get_cart_items'),
-    path('add/', views.add_to_cart, name='add_to_cart')
+    path('add/', views.add_to_cart, name='add_to_cart'),
+    path('remove/', views.remove_from_cart, name='remove_from_cart')
 ]
