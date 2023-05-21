d6e5ac4a8c543e6a704d75167bda43b62465a38f
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 23:59:49 +0300

    [fix] serializers in orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+
+from core.backends import User
 from .models import Order
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = ['user', 'order_time', 'total_price', 'delivery_time', 'delivery_method', 'payment_method']
