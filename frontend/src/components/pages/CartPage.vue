ab8319b9af3e07cb8677ecc6c3172e59a0dc88c6
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 21:33:13 +0300

    [add] CartPage

--- frontend/src/components/pages/CartPage.vue
+++ frontend/src/components/pages/CartPage.vue
@@ -1,11 +1,49 @@
-<template></template>
+<template>
+  <div>
+    <h1>Cart</h1>
+    <div v-if="cartItems.length === 0">
+      <p>Your cart is empty.</p>
+    </div>
+    <div v-else>
+      <ul>
+        <li v-for="item in cartItems" :key="item.id">
+          <p>Product: {{ item.product }}</p>
+          <p>Quantity: {{ item.quantity }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "CartPage"
-}
+  data() {
+    return {
+      cartItems: [],
+    };
+  },
+  mounted() {
+    this.getCartItems();
+  },
+  methods: {
+    getCartItems() {
+      const token = localStorage.getItem('access_token');
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      console.log(headers)
+
+      axios
+        .get('http://localhost:8000/api/cart/', { headers })
+        .then((response) => {
+          this.cartItems = response.data;
+        })
+        .catch((error) => {
+          console.error('Failed to fetch cart items', error);
+        });
+    },
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
