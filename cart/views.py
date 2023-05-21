ccea8a8f5f08b5d9441a53c56f735ec273c9f03a
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 21:33:36 +0300

    [add] CartView in views

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,19 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from core.auth import CustomJWTAuthentication
+from .models import Cart
 from .serializers import CartSerializer
+
+
+class CartView(APIView):
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        cart_items = Cart.objects.filter(user=user)
+        serializer = CartSerializer(cart_items, many=True)
+        return Response(serializer.data)
 
 
 class CartAddView(APIView):
@@ -13,11 +25,9 @@ class CartAddView(APIView):
     def post(self, request):
         data = request.data.copy()
         data['user'] = request.user.id
-        print(data)
         serializer = CartSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response({'success': True, 'message': 'Product added to cart successfully'})
-        print(serializer.errors)
         return Response({'success': False, 'message': serializer.errors})
 
