a06653722770a92fc2742137f0f28762bc73e839
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 23:06:05 +0300

    [add] models in orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_time = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_time = models.DateTimeField()
+    delivery_method = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    delivery_address = models.CharField(max_length=255)
