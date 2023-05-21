7949e5a3ef366d609d29be6f664def90419b8b9f
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 23:05:14 +0300

    [add] views in orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.auth import CustomJWTAuthentication
+from .serializers import OrderSerializer
+
+
+class OrderView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [CustomJWTAuthentication]
+
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({'success': True, 'message': 'Order created successfully'})
+        return Response({'success': False, 'message': serializer.errors})
