0896af398f8bcc3e51a9c678fabdb6b6d876ee17
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 14:51:36 +0300

    [fix] UserProfile

--- frontend/src/components/elements/UserProfile.vue
+++ frontend/src/components/elements/UserProfile.vue
@@ -3,11 +3,12 @@
     <h2>User Profile</h2>
     <div v-if="loading">Loading...</div>
     <div v-else>
-      <p>Email: {{ user.email }}</p>
-      <p>Имя: {{ user.first_name }}</p>
-      <p>Фамилия: {{ user.last_name }}</p>
-      <p>Номер телефона: {{ user.phone_number }}</p>
-      <p>Адрес: {{ user.address }}</p>
+      <p>Email: {{ user.email || 'null' }}</p>
+      <p>Имя: {{ user.first_name || 'null' }}</p>
+      <p>Фамилия: {{ user.last_name || 'null' }}</p>
+      <p>Номер телефона: {{ user.phone_number || 'null' }}</p>
+      <p>Адрес: {{ user.address || 'null' }}</p>
+      <button @click="editProfile">Редактировать</button>
     </div>
   </div>
 </template>
@@ -28,8 +29,8 @@ export default {
   },
   methods: {
     fetchUserProfile() {
-      const token = localStorage.getItem('access_token')
-      console.log(token)
+      const token = localStorage.getItem('access_token');
+      console.log(token);
       axios
         .get('http://localhost:8000/api/core/profile/', {
           headers: {
@@ -44,6 +45,9 @@ export default {
           console.log('Failed to fetch user profile', error);
           this.loading = false;
         });
+    },
+    editProfile() {
+      this.$router.push('/profile/edit/');
     },
   },
 };
