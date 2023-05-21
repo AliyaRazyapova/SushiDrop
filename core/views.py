90252a8e014c38ef0bd0de4a7bc3b3971abe4833
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 03:15:28 +0300

    [fix] UserProfileView in views

--- core/views.py
+++ core/views.py
@@ -77,6 +77,10 @@ class UserProfileView(APIView):
         user = request.user
         data = {
             'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'phone_number': user.phone_number,
+            'address': user.phone_number
         }
         return Response(data)
 
