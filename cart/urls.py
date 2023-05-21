c4558f92b27964740288769acf81a812213f266f
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 20:46:06 +0300

    [add] CartAddView in urls cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from cart import views
 
 urlpatterns = [
-    path('get/', views.get_cart_items, name='get_cart_items'),
-    path('add/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('clear/', views.clear_cart, name='clear_cart')
+    path('add/', views.CartAddView.as_view())
 ]
