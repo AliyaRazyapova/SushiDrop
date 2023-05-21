cffaa62be92c62939c341a31fbc85f5086630cc3
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-21 20:42:25 +0300

    [add] CartAddView in views

--- cart/views.py
+++ cart/views.py
@@ -1,52 +1,23 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import Cart
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from core.auth import CustomJWTAuthentication
 from .serializers import CartSerializer
-from django.contrib.auth.decorators import login_required
 
 
-@login_required
-@csrf_exempt
-def get_cart_items(request):
-    user = request.user
-    cart_items = Cart.objects.filter(user=user)
-    serializer = CartSerializer(cart_items, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class CartAddView(APIView):
+    authentication_classes = [CustomJWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
+    def post(self, request):
+        data = request.data.copy()
+        data['user'] = request.user.id
+        print(data)
+        serializer = CartSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'success': True, 'message': 'Product added to cart successfully'})
+        print(serializer.errors)
+        return Response({'success': False, 'message': serializer.errors})
 
-@login_required
-@csrf_exempt
-def add_to_cart(request):
-    user = request.user
-    product_id = request.POST.get('product_id')
-    quantity = int(request.POST.get('quantity'))
-
-    cart_item = Cart.objects.filter(user=user, product_id=product_id).first()
-    if cart_item:
-        cart_item.quantity += quantity
-        cart_item.save()
-    else:
-        cart_item = Cart.objects.create(user=user, product_id=product_id, quantity=quantity)
-
-    serializer = CartSerializer(cart_item)
-    return JsonResponse(serializer.data)
-
-
-@login_required
-@csrf_exempt
-def remove_from_cart(request, cart_item_id):
-    user = request.user
-    try:
-        cart_item = Cart.objects.get(id=cart_item_id, user=user)
-        cart_item.delete()
-        return JsonResponse({'detail': 'Item removed from cart successfully'})
-    except Cart.DoesNotExist:
-        return JsonResponse({'detail': 'Item not found in cart'}, status=404)
-
-
-@login_required
-@csrf_exempt
-def clear_cart(request):
-    user = request.user
-    Cart.objects.filter(user=user).delete()
-    return JsonResponse({'detail': 'Cart cleared successfully'})
