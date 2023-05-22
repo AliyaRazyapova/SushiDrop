d902a938a9f93b1615f177c65fb53c89baeb6180
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-23 02:50:34 +0300

    [add] models in discounts

--- discounts/models.py
+++ discounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Discount(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
