de7fa8d54bd6c89a258a84388569fc7c12835ec4
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-22 23:00:46 +0300

    [add] swagger

--- sushidrop/urls.py
+++ sushidrop/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
     path('api/products/', include('products.urls')),
