a9660d432f8353d362c82ad5cf30d20a49533333
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-23 14:45:57 +0300

    [fix] serializers in discounts

--- discounts/serializers.py
+++ discounts/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
+
+from products.serializers import ProductSerializer
 from .models import Discount
 
 
 class DiscountSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+
     class Meta:
         model = Discount
         fields = '__all__'
