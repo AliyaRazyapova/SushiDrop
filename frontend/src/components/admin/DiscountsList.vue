77bef0e1dc3d3c63a61f4883678550ae755e9d22
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-23 14:46:14 +0300

    [add] price in DiscountsList

--- frontend/src/components/admin/DiscountsList.vue
+++ frontend/src/components/admin/DiscountsList.vue
@@ -3,8 +3,10 @@
     <h1>Discounts</h1>
     <ul>
       <li v-for="discount in discounts" :key="discount.id">
-        <p>Product: {{ discount.product }}</p>
+        <p>Product: {{ discount.product.name }}</p>
         <p>Discount Percentage: {{ discount.discount_percentage }}</p>
+        <p>Price: {{ discount.product.price }}</p>
+        <p>Discounted Price: {{ calculateDiscountedPrice(discount) }}</p>
         <p>Start Date: {{ discount.start_date }}</p>
         <p>End Date: {{ discount.end_date }}</p>
       </li>
@@ -35,6 +37,12 @@ export default {
         .catch(error => {
           console.error('Failed to fetch discounts', error);
         });
+    },
+    calculateDiscountedPrice(discount) {
+      const originalPrice = discount.product.price;
+      const discountPercentage = discount.discount_percentage;
+      const discountedPrice = originalPrice * (1 - discountPercentage / 100);
+      return discountedPrice.toFixed(2);
     },
   },
 };
