d20e5cf9b53c84f72bfa42a786e7e72afffaa242
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-23 03:00:13 +0300

    [fix] DiscountsForm

---
+++ frontend/src/components/admin/DiscountsForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Discounts</h1>
+    <ul>
+      <li v-for="discount in discounts" :key="discount.id">
+        <p>Product: {{ discount.product.name }}</p>
+        <p>Discount Percentage: {{ discount.discount_percentage }}</p>
+        <p>Start Date: {{ discount.start_date }}</p>
+        <p>End Date: {{ discount.end_date }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'DiscountsForm',
+  data() {
+    return {
+      discounts: [],
+    };
+  },
+  mounted() {
+    this.getDiscounts();
+  },
+  methods: {
+    getDiscounts() {
+      axios.get('http://localhost:8000/api/discounts/')
+        .then(response => {
+          this.discounts = response.data;
+        })
+        .catch(error => {
+          console.error('Failed to fetch discounts', error);
+        });
+    },
+  },
+};
+</script>
