39be1851a3e443c39d6510d4a7b2e98eb0baa0d0
AliyaRazyapova <a.raziapova@mail.ru>
2023-05-24 08:39:21 +0300

    [fix] CreateProduct

--- frontend/src/components/admin/CreateProduct.vue
+++ frontend/src/components/admin/CreateProduct.vue
@@ -59,7 +59,7 @@ export default {
   methods: {
     async loadCategories() {
       try {
-        const response = await fetch('http://localhost:8000/api/categories/')
+        const response = await fetch('http://localhost:8000/api/products/categories/')
         const data = await response.json()
         this.categories = data
       } catch (error) {
