b388a70f6be72485da4dd6ac61bcf9e9cd155d8b
AliyaRazyapova <a.raziapova@mail.ru>
2023-05-24 16:33:33 +0300

    [add] reset_password in urls

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('reset-password/', views.reset_password, name='reset_password'),
 ]
