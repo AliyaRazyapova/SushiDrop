345ed132285f2b27c083d91f3358aa2daa051838
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 15:19:26 +0300

    [add] UserProfilePage

---
+++ frontend/src/components/pages/UserProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <hea-der />
+  <category-list />
+  <user-profile />
+</template>
+
+<script>
+import UserProfile from "@/components/elements/UserProfile";
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+
+export default {
+  name: "UserProfilePage",
+  components: {CategoryList, HeaDer, UserProfile}
+}
+</script>
