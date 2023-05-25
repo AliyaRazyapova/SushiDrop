3540594efa199ad8588a5126152d3b19a8eaf9ff
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 18:12:17 +0300

    [fix] VKForm

--- frontend/src/components/forms/VKForm.vue
+++ frontend/src/components/forms/VKForm.vue
@@ -1,13 +1,21 @@
 <template>
-  <button class="vk-button" @click="authorize">Авторизация через VK</button>
+  <div>
+    <input type="email" v-model="email" name="email" placeholder="Введите ваш email">
+    <button class="vk-button" @click="authorize">Авторизация через VK</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "VKForm",
+  data() {
+    return {
+      email: ""
+    };
+  },
   methods: {
     authorize() {
-      const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51639163&redirect_uri=http://localhost:8000/auth/vk/callback&response_type=code&scope=email`;
+      const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51639163&redirect_uri=http://localhost:8000/auth/vk/callback&response_type=code&scope=email&state=${encodeURIComponent(this.email)}`;
       window.location.href = vkAuthUrl;
     }
   },
@@ -36,11 +44,6 @@ export default {
 
 .vk-button:hover {
   background-color: #375E91;
-}
-
-.vk-button:focus {
-  outline: none;
-  box-shadow: 0 0 0 2px #fff, 0 0 0 4px #4A76A8;
 }
 </style>
 
