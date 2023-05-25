d2850b5dd828f2de8e233827fec5850561d81935
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 21:36:14 +0300

    [fix] serializers in orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,10 +1,37 @@
 from rest_framework import serializers
 
-from core.backends import User
-from .models import Order
+from products.models import Product
+from .models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'quantity', 'total_price']
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True)
+
     class Meta:
         model = Order
-        fields = ['user', 'order_time', 'total_price', 'delivery_time', 'delivery_method', 'payment_method']
+        fields = ['user', 'order_time', 'total_price', 'delivery_time', 'delivery_method',
+                  'payment_method', 'delivery_address', 'order_items']
+
+    def create(self, validated_data):
+        order_items_data = validated_data.pop('order_items')
+        order = Order.objects.create(**validated_data)
+        for order_item_data in order_items_data:
+            product_id = order_item_data['product'].id  # Получить идентификатор продукта
+            quantity = order_item_data['quantity']
+            product = Product.objects.get(id=product_id)
+
+            order_item = OrderItem(
+                order=order,
+                product=product,
+                quantity=quantity,
+                total_price=product.price * quantity
+            )
+            order_item.save()
+
+        return order
