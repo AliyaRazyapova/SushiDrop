c22b230e42adbd89c70f23354433b447e2eb460b
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 16:38:54 +0300

    [add] ResetPasswordPage

---
+++ frontend/src/components/pages/ResetPasswordPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <hea-der />
+  <category-list />
+  <reset-password />
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+import ResetPassword from "@/components/forms/ResetPassword";
+
+export default {
+  name: "ResetPasswordPage",
+  components: {ResetPassword, CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+
+</style>
