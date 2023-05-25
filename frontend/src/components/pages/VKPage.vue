926db77bf6bde5280db4edaae3a8bc4db3cda188
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 18:28:56 +0300

    [add] VKPage

---
+++ frontend/src/components/pages/VKPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <hea-der />
+  <category-list />
+  <v-k-form />
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+import VKForm from "@/components/forms/VKForm";
+
+export default {
+  name: "VKPage",
+  components: {VKForm, CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+
+</style>
