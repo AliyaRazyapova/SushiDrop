a8586da6d772d4116fc796831662560ba54be514
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 00:50:15 +0300

    [add] models in couriers

--- couriers/models.py
+++ couriers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Courier(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class CourierOrder(models.Model):
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    order = models.ForeignKey('orders.Order', on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"Courier: {self.courier.name}, Order: {self.order.id}"
