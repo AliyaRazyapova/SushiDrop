84614ff3314796462713e4157c8f4317d9cf8423
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 15:10:01 +0300

    [add] styles in EditProfile

--- frontend/src/components/elements/EditProfile.vue
+++ frontend/src/components/elements/EditProfile.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
-    <h2>Edit Profile</h2>
-    <form @submit.prevent="updateProfile">
-      <div>
-        <label for="first_name">First Name:</label>
-        <input type="text" id="first_name" v-model="user.first_name" required>
+  <div class="container">
+    <h2>Редактирование профиля</h2>
+    <form @submit.prevent="updateProfile" class="edit-form">
+      <div class="form-group">
+        <label for="first_name" class="label-left">Имя:</label>
+        <input type="text" id="first_name" v-model="user.first_name" required class="input-right">
       </div>
-      <div>
-        <label for="last_name">Last Name:</label>
-        <input type="text" id="last_name" v-model="user.last_name" required>
+      <div class="form-group">
+        <label for="last_name" class="label-left">Фамилия:</label>
+        <input type="text" id="last_name" v-model="user.last_name" required class="input-right">
       </div>
-      <div>
-        <label for="phone_number">Phone Number:</label>
-        <input type="text" id="phone_number" v-model="user.phone_number" required>
+      <div class="form-group">
+        <label for="phone_number" class="label-left">Номер телефона:</label>
+        <input type="text" id="phone_number" v-model="user.phone_number" required class="input-right">
       </div>
-      <div>
-        <label for="address">Address:</label>
-        <input type="text" id="address" v-model="user.address" required>
+      <div class="form-group">
+        <label for="address" class="label-left">Адрес:</label>
+        <input type="text" id="address" v-model="user.address" required class="input-right">
       </div>
-      <div>
-        <button type="submit">Save</button>
+      <div class="form-group">
+        <button type="submit" class="save-button">Сохранить</button>
       </div>
     </form>
   </div>
@@ -78,3 +78,57 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 40px;
+  }
+
+  .edit-form {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .form-group {
+    margin-bottom: 10px;
+  }
+
+  .label-left {
+    text-align: left;
+    font-weight: bold;
+    display: inline-block;
+    width: 150px;
+  }
+
+  .input-right {
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    text-align: right;
+    display: inline-block;
+    width: 200px;
+  }
+
+  .save-button {
+    padding: 5px 10px;
+    background-color: #F52341;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+    margin-top: 20px;
+
+    font-size: 14px;
+  }
+
+  .save-button:hover {
+    background-color: #F52341;
+  }
+</style>
