07747b317d7cc27caf43f5df92fc418f90d25880
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 16:22:50 +0300

    [add] DiscountsPage

---
+++ frontend/src/components/pages/DiscountsPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <hea-der />
+  <category-list />
+  <discounts-list />
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+import DiscountsList from "@/components/pages/DiscountsList";
+export default {
+  name: "DiscountsPage",
+  components: {DiscountsList, CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+
+</style>
