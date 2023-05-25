e226efd15603f085d99e4cc5047802f7622e5fe6
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 00:33:33 +0300

    [fix] serializers in orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -22,15 +22,16 @@ class OrderSerializer(serializers.ModelSerializer):
         order_items_data = validated_data.pop('order_items')
         order = Order.objects.create(**validated_data)
         for order_item_data in order_items_data:
-            product_id = order_item_data['product'].id  # Получить идентификатор продукта
+            product_id = order_item_data['product'].id
             quantity = order_item_data['quantity']
+            total_price = order_item_data['total_price']
             product = Product.objects.get(id=product_id)
 
             order_item = OrderItem(
                 order=order,
                 product=product,
                 quantity=quantity,
-                total_price=product.price * quantity
+                total_price=total_price * quantity
             )
             order_item.save()
 
