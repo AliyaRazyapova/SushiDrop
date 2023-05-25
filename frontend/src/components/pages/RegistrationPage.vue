7b62fdab0a144500e2afa8684de02cd879c66ba5
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 12:52:32 +0300

    [add] category list in RegistrationPage

--- frontend/src/components/pages/RegistrationPage.vue
+++ frontend/src/components/pages/RegistrationPage.vue
@@ -1,15 +1,18 @@
 <template>
   <hea-der />
+  <category-list />
   <registration-form />
 </template>
 
 <script>
 import HeaDer from "@/components/elements/HeaDer";
 import RegistrationForm from "@/components/forms/RegistrationForm";
+import CategoryList from "@/components/elements/CategoryList";
 
 export default {
   name: "RegistrationPage",
   components: {
+    CategoryList,
     RegistrationForm,
     HeaDer
   }
