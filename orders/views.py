e93d3f9898f8819e47e07e217175c74a2a2ead68
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 21:36:03 +0300

    [fix] views in orders

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,13 @@
+from django.db import transaction
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
 from core.auth import CustomJWTAuthentication
+from products.models import Product
+from .models import OrderItem
 from .serializers import OrderSerializer
 
 
@@ -20,6 +23,25 @@ class OrderView(APIView):
     def post(self, request):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            order_items = request.data.get('order_items', [])
+
+            with transaction.atomic():
+                order = serializer.save()
+
+                order_items_data = []
+                for item in order_items:
+                    product_id = item.get('product')
+                    quantity = item.get('quantity')
+                    product = Product.objects.get(id=product_id)
+
+                    order_item = OrderItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                        total_price=product.price * quantity
+                    )
+                    order_items_data.append(order_item)
+
             return Response({'success': True, 'message': 'Order created successfully'})
-        return Response({'success': False, 'message': serializer.errors})
+
+        return Response({'success': False, 'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
