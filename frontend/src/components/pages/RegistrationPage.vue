cff64c123e6c7c17478ed54d2601561d46d8f3e2
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 12:49:56 +0300

    [add] RegistrationPage

---
+++ frontend/src/components/pages/RegistrationPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <hea-der />
+  <registration-form />
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import RegistrationForm from "@/components/forms/RegistrationForm";
+
+export default {
+  name: "RegistrationPage",
+  components: {
+    RegistrationForm,
+    HeaDer
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
