a33de3332eab88252c0cdc21d0d684f14e8f7133
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-25 15:21:50 +0300

    [add] EditProfilePage

---
+++ frontend/src/components/pages/EditProfilePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <hea-der />
+  <category-list />
+  <edit-profile />
+</template>
+
+<script>
+import HeaDer from "@/components/elements/HeaDer";
+import CategoryList from "@/components/elements/CategoryList";
+import EditProfile from "@/components/elements/EditProfile";
+export default {
+  name: "EditProfilePage",
+  components: {EditProfile, CategoryList, HeaDer}
+}
+</script>
+
+<style scoped>
+
+</style>
