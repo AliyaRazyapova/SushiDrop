a64780d159433259130fa5f643019599b1d8fc99
AliyaRazyapova <a.raziapova@mail.ru>
2023-05-26 12:42:57 +0300

    [fix] tasks

--- core/tasks.py
+++ core/tasks.py
@@ -1,4 +1,6 @@
 from celery import Celery
+from celery import shared_task
+
 from django.core.mail import send_mail
 
 app = Celery('sushidrop', broker='redis://localhost:6379/0')
@@ -12,3 +14,15 @@ def send_password_reset_email(email, reset_token):
     recipient_email = email
 
     send_mail(subject, message, sender_email, [recipient_email])
+
+
+@shared_task
+def send_discount_emails():
+    from core.models import User
+
+    users = User.objects.all()
+
+    for user in users:
+        email_subject = 'Скидки сегодня!'
+        email_message = 'У нас сегодня особые скидки только для вас. Заходите на наш сайт и смотрите актуальные предложения.'
+        send_mail(email_subject, email_message, '2458750@gmail.com', [user.email])
