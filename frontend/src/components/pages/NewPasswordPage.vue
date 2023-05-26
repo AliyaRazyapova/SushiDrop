519309bff5de3b1747314f6ea974e75fe13402e8
AliyaRazyapova <a.raziapova@mail.ru>
2023-05-26 13:46:30 +0300

    [add] NewPasswordPage

---
+++ frontend/src/components/pages/NewPasswordPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <hea-der />
+  <category-list />
+  <new-password />
+</template>
+
+<script setup>
+
+import HeaDer from "@/components/elements/HeaDer.vue";
+import CategoryList from "@/components/elements/CategoryList.vue";
+import NewPassword from "@/components/forms/NewPassword.vue";
+</script>
+
+<style scoped>
+
+</style>
