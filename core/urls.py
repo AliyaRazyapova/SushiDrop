7cd87f0d2bcafc7151a53adee456b3c7919d7ae3
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 08:37:39 +0300

    [add] auth in urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('auth/', views.UserRoleView.as_view(), name='check_authentication'),
     path('reset-password/', views.reset_password, name='reset_password'),
     path('reset-password-confirm/', views.reset_password_confirm)
 ]
