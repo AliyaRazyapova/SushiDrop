7e458f9896f5e9ef69fbabf5e2716c24862eb56b
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 08:39:06 +0300

    [add] DiscountUpdateView in urls

--- discounts/urls.py
+++ discounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.DiscountView.as_view(), name='discounts'),
     path('list/', views.DiscountListView.as_view(), name='discounts_list'),
-    path('add/', views.DiscountCreateView.as_view(), name='create_discounts')
+    path('add/', views.DiscountCreateView.as_view(), name='create_discounts'),
+    path('<int:discount_id>/', views.DiscountUpdateView.as_view(), name='edit_discounts')
 ]
