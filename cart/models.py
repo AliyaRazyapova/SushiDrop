6893e10a98e76edfb66b00bd45a82fe06a81ce9f
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 03:59:49 +0300

    [fix] models in cart

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,6 @@ from products.models import Product
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # discount = models.ForeignKey(Discount, default=0, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
