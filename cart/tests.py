786cd166f9724d03ca823a163fa963ed5e91ce27
Razyapova Aliya <a.raziapova@mail.ru>
2023-05-26 04:00:04 +0300

    [add] test get cart items

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,15 @@
+from core.models import User
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class CartViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='test@example.com', password='test123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_get_cart_items(self):
+        response = self.client.get('/api/cart/', format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
