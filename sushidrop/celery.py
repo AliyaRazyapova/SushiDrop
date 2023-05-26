9469e61769e42cbdafab3bb541be2a24daffd7a3
AliyaRazyapova <a.raziapova@mail.ru>
2023-05-26 12:43:26 +0300

    [fix] celery

--- sushidrop/celery.py
+++ sushidrop/celery.py
@@ -1,4 +1,10 @@
+import os
 from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sushidrop.settings')
 
 app = Celery('sushidrop', broker='redis://localhost:6379/0')
 app.autodiscover_tasks()
+
+from core.tasks import send_discount_emails
